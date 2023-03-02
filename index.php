7711fbcc1ed4fcb0360577045ead17c8a129cd7b
Vinicius Ferreira <vinigferreira17@gmail.com>
2023-03-02 19:10:18 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "<h1>Ola mundo</h1>";
+?>
+
+<?php
+    echo "<h2>8 + 8 = " . (8 + 8) . "</h2>";
+    echo "<h2>8 - 8 = " . (8 - 8) . "</h2>";
+    echo "<h2>8 * 1 = " . (8 * 1) . "</h2>";
+    echo "<h2>8 / 2 = " . (8 / 2) . "</h2>";
+?>
+</body>
+</html>
